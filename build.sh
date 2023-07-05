122eadc89c39aabb5abacb2f5c928230c5466da2
yhirose <yuji.hirose.bug@gmail.com>
2023-07-04 23:28:36 -0400

    Fixed build problem with bison and flex

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+bison -d -y grammar.y
+flex -l lex.l
 clang++ --std=c++17 -Wno-deprecated-declarations -Wno-deprecated -o atc extern.cpp graphics.cpp input.cpp list.cpp log.cpp main.cpp tunable.cpp update.cpp y.tab.c lex.yy.c -lcurses -ll
